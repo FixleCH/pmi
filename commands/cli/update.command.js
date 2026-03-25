4752e300065c6e13dbe437205f9517f92eb533e9
Fixle <249085542+FixleCH@users.noreply.github.com>
2026-03-25 15:26:23 +0100

    Delete commands/cli/update.command.js

--- commands/cli/update.command.js
+++
@@ -1,8 +0,0 @@
-import chalk from 'chalk';
-import { updatePackages } from '../../src/core/package.manager.command.js';
-
-export async function updateCommand() {
-  console.log(chalk.yellow('PMI: Updating dependencies...'));
-  await updatePackages();
-  console.log(chalk.green('PMI: Update completed!'));
-}
