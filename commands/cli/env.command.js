6213e6ad74c11517c247cbb75b7f8dece451d166
Fixle <249085542+FixleCH@users.noreply.github.com>
2026-03-25 15:25:55 +0100

    Delete commands/cli/env.command.js

--- commands/cli/env.command.js
+++
@@ -1,7 +0,0 @@
-import { doctorCommand } from './doctor.command.js';
-import chalk from 'chalk';
-
-export async function envCommand() {
-  console.log(chalk.magenta('PMI: Environment summary\n'));
-  await doctorCommand();
-}
